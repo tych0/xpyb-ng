d6ba71e2ecb00f43b78757cea451cdd03ede8d44
Tycho Andersen <tycho@tycho.ws>
2012-01-23 19:37:25 -0600

    Fix the build system to generate a pkgconfig file and install the header files.

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
-import shutil, os
+import shutil, os, sys, errno
+from collections import namedtuple
 from distutils.core import setup, Extension
 from distutils.command.build_ext import build_ext as _build_ext
+from distutils.command.install_headers import install_headers
+from distutils.sysconfig import get_config_var, PREFIX
+from shutil import copyfile
 from tools import py_client
+
+VERSION="1.3"
 
 CFLAGS=["-Wall"]
 
@@ -15,7 +21,6 @@ def find_xcb():
         if os.path.isdir(i):
             return i
     raise ValueError("Could not detect xcb protocol definition location...")
-
 
 xmlfiles = [
     "bigreq", "composite", "damage", "dpms", "glx",
@@ -38,20 +43,75 @@ ext_modules = [
     )
 ]
 
-
 class build_ext(_build_ext):
     def run(self):
         for i in xmlfiles:
             py_client.build(os.path.join(find_xcb(), "%s.xml"%i))
         return _build_ext.run(self)
 
+# make xpyb.pc file
+class PCOpt(object):
+    def __init__(self, replace_str, val):
+        self.replace_str = replace_str
+        self.val = val
+
+def gen_pc():
+    pc_opts = {
+        '--prefix': PCOpt('@prefix@', '/usr/local'),
+        '--exec-prefix': PCOpt('@exec_prefix@', '${prefix}'),
+        '--install-lib': PCOpt('@libdir@', '${exec_prefix}/lib'),
+        '--install-headers': PCOpt('@includedir@', '${prefix}/include/python2.7/xpyb'),
+        # please don't actually use this :-)
+        '--dont-set-the-xcb-version': PCOpt('@PACKAGE_VERSION@', VERSION),
+    }
+    def override_arg(arg):
+        for (k, v) in pc_opts.items():
+            if arg.startswith(k):
+                try:
+                    v.val = arg.split('=')[1]
+                except IndexError:
+                    pass
+    for arg in sys.argv:
+        override_arg(arg)        
+
+    with open('xpyb.pc.in') as in_:
+        pc = in_.read()
+    with open('xpyb.pc', 'w') as out:
+        for opt in pc_opts.values():
+            pc = pc.replace(opt.replace_str, opt.val)
+        out.write(pc)
+
+    # if we're not installing, don't install the .pc
+    if 'install' not in sys.argv:
+        return
+    
+    def resolve(path):
+        """ Resolve a path through pkgconfig variables. """
+        for opt in pc_opts.values():
+            name = '${' + opt.replace_str[1:-1] + '}' # strip off @s
+            if name in path:
+                path = path.replace(name, opt.val)
+                path = resolve(path)
+        return path
+
+    pkgconfig = os.path.join(resolve(pc_opts['--install-lib'].val), 'pkgconfig')
+    try:
+        os.makedirs(pkgconfig)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            pass
+        else:
+            raise
+    copyfile('xpyb.pc', os.path.join(pkgconfig, 'xpyb.pc'))
 
 setup(
     name = 'xpyb',
-    version = "1.3",
+    version = VERSION,
     ext_modules = ext_modules,
     packages = ["xcb"],
     cmdclass = {
         "build_ext": build_ext
-    }
+    },
+    headers=['xcb/xpyb.h'],
 )
+gen_pc()
